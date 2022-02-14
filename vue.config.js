1842ad1f7e1142df12b3b04e043211f37bdfc8b6
tolunayakbulut <qame1375>
2022-02-15 02:51:13 +0300

    added eslintignore

--- vue.config.js
+++ vue.config.js
@@ -9,23 +9,22 @@
 
 
 module.exports = {
-  publicPath: '/',
-  transpileDependencies: [
-    'vue-echarts',
-    'resize-detector'
-  ],
-  configureWebpack: {
-    optimization: {
-      splitChunks: {
-        chunks: 'all'
-      }
+    publicPath: '/',
+    transpileDependencies: [
+        'vue-echarts',
+        'resize-detector'
+    ],
+    configureWebpack: {
+        optimization: {
+            splitChunks: {
+                chunks: 'all'
+            }
+        }
     }
-  }
-  // devServer: {
-  //   overlay: {
-  //     warnings: true,
-  //     errors: true
-  //   }
-  // }
+    // devServer: {
+    //   overlay: {
+    //     warnings: true,
+    //     errors: true
+    //   }
+    // }
 }
-
